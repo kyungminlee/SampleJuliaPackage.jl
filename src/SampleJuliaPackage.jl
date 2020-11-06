0bce53be0ca6d75da3a5bf379b55605559c612c2
Kyungmin Lee <kyungmin.lee.42@gmail.com>
2020-11-06 10:19:31 -0500

    Add workflow

--- src/SampleJuliaPackage.jl
+++ src/SampleJuliaPackage.jl
@@ -2,4 +2,31 @@ module SampleJuliaPackage
 
 greet() = print("Hello World!")
 
+export test0, test0i, test1, test1i
+
+
+function test0(a, b)
+  return a[1] + b[1]
+end
+
+@inline function test0i(a, b)
+  return a[1] + b[1]
+end
+
+function test1(a, b)
+  @boundscheck (@assert a==b)
+  return a[1] + b[1]
+end
+
+@inline function test1i(a ::Vector{S}, b ::Vector{T}) where {S, T}
+  @boundscheck (@assert a==b)
+  return a[1] + b[1]
+end
+
+@inline function test2i(a ::Vector{Int}, b ::Vector{Int})
+  @boundscheck (@assert a==b)
+  return a[1] + b[1]
+end
+
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,5 @@ using Test
 
 @testset "SampleJuliaPackage.jl" begin
     # Write your own tests here.
+    @test test0([1,2,3], [4,5,6]) == 5
 end
